94a9187425d13f48c479b254363c291199990f95
Pavel Egorov <xoposhiy@mail.ru>
2017-07-26 13:02:12 +0500

    remove bad dependencies

--- lib/Firebase/FbClient.cs
+++ lib/Firebase/FbClient.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Firebase.Auth;
 using Firebase.Database;
-using NUnit.Framework;
+using Firebase.Database.Query;
 
-namespace lib
+namespace lib.Firebase
 {
     public class FbClient
     {
-        private FirebaseClient fb;
+        private readonly FirebaseClient fb;
 
         public FbClient()
         {
@@ -19,12 +19,15 @@ namespace lib
 
         public IEnumerable<Problem> GetProblems()
         {
-            return fb.Child("problems").OnceAsync<Problem>().ConfigureAwait(false).GetAwaiter().GetResult().Select(f => f.Object);
+            return fb.Child("problems").OnceAsync<Problem>().ConfigureAwait(false).GetAwaiter().GetResult()
+                .Select(f => f.Object);
         }
 
         private static async Task<FirebaseClient> Authenticate()
         {
             var auth = new FirebaseAuthProvider(new FirebaseConfig("AIzaSyBRYZvtAg1Vm5fZZ-r80vCISm0A8IhA7vM"));
+            
+            //change apssword in firebase console before repo publishing!
             FirebaseAuthLink authLink = await auth.SignInWithEmailAndPasswordAsync("pe@kontur.ru", "W1nnerzz");
             var fb = new FirebaseClient(
                 "https://icfpc2017.firebaseio.com",
@@ -34,26 +37,21 @@ namespace lib
                 });
             return fb;
         }
+
+        public void SaveProblem(Problem problem)
+        {
+            fb.Child("problems").Child(problem.Id.ToString()).PutAsync(problem).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
     }
 
     public class Problem
     {
-        public int k;
+        public int Id;
+        public DateTime Date;
 
         public override string ToString()
         {
-            return $"{nameof(k)}: {k}";
-        }
-    }
-
-    [TestFixture]
-    public class FbClient_Should
-    {
-        [Test]
-        public void DoSomething_WhenSomething()
-        {
-            var ps = new FbClient().GetProblems();
-            Console.WriteLine(string.Join(" ", ps));
+            return $"{nameof(Id)}: {Id} {Date}";
         }
     }
 }
---
+++ lib/Firebase/FbClient_Tests.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Linq;
+using System.Threading;
+using MoreLinq;
+using NUnit.Framework;
+using Shouldly;
+
+namespace lib.Firebase
+{
+    [TestFixture]
+    public class FbClient_Tests
+    {
+        [Test]
+        public void FirebaseSmokeTest()
+        {
+            var client = new FbClient();
+            var problem = new Problem { Id = 42, Date = DateTime.Now };
+            client.SaveProblem(problem);
+            var ps = client.GetProblems().ToList();
+            ps.ShouldContain(p => p.Id == 42);
+            Console.WriteLine(ps.ToDelimitedString(","));
+            Console.WriteLine(problem);
+        }
+    }
+}
