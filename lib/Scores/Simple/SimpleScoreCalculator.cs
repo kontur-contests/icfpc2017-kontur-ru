b752c6e87d7de0bbaee206e2200b8c93eb19765e
kungurtsev <kungurtsev@skbkontur.ru>
2017-08-05 01:45:24 +0500

    normal calculator

--- lib/Scores/Simple/SimpleScoreCalculator.cs
+++ lib/Scores/Simple/SimpleScoreCalculator.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using lib.GraphImpl;
 
 namespace lib.Scores.Simple
 {
@@ -8,51 +6,21 @@ namespace lib.Scores.Simple
     {
         public long GetScore(int punter, Map map)
         {
-            var distances = FloydAlgorithm.Run(map);
+            var graph = new Graph(map);
+            var distCalc = new MineDistCalculator(graph);
+            var minesCalc = new ConnectedCalculator(graph, punter);
 
-            var punterRivers = map.Rivers
-                .Where(e => e.Owner == punter)
-                .ToArray();
-
-            var mapSize = map.Sites.Length;
-
-            var disjointSetUnion = BuildDisjointSetUnion(mapSize, punterRivers);
-
-            var allSites = GetAllSites(map, disjointSetUnion).ToArray();
-
-            return allSites
-                .Sum(e => distances[e.Item1][e.Item2] * distances[e.Item1][e.Item2]);
-        }
-
-        private IEnumerable<Tuple<int, int>> GetAllSites(Map map, DisjointSetUnion disjointSetUnion)
-        {
-            foreach (var mine in map.Mines)
+            long res = 0;
+            foreach (var vertex in graph.Vertexes)
             {
-                foreach (var site in map.Sites)
+                var mines = minesCalc.GetConnectedMines(vertex.Key);
+                foreach (var mine in mines)
                 {
-                    if (site.Id == mine)
-                    {
-                        continue;
-                    }
-
-                    if (disjointSetUnion.SameSet(site.Id, mine))
-                    {
-                        yield return Tuple.Create(site.Id, mine);
-                    }
+                    long dist = distCalc.GetDist(mine, vertex.Key);
+                    res += dist * dist;
                 }
             }
-        }
-
-        private static DisjointSetUnion BuildDisjointSetUnion(int mapSize, River[] punterRivers)
-        {
-            var disjointSetUnion = new DisjointSetUnion(mapSize);
-
-            foreach (var river in punterRivers)
-            {
-                disjointSetUnion.Add(river.Target, river.Source);
-            }
-
-            return disjointSetUnion;
+            return res;
         }
     }
 }
