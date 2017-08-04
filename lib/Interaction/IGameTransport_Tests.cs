d44e70093faab82e046c3564e1cad317dab3977e
s.fedyanin <s.fedyanin@skbkontur.ru>
2017-08-04 20:28:49 +0500

    Добавил тест на IGameTransport

---
+++ lib/Interaction/IGameTransport_Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using FakeItEasy;
+using NUnit.Framework;
+
+namespace lib.Interaction
+{
+    [TestFixture]
+    public class IGameTransport_Tests
+    {
+        [Test]
+        public void TestHandShake()
+        {
+            var x = "a";
+            Console.WriteLine($"{{\"me\":\"{x}\"}}");
+            var transport = A.Fake<OfflineHighTransport>();
+            var gameTransport = new OnlineInteraction(transport);
+            A.CallTo(() => transport.Read()).Returns("{\"you\":\"player\"}");
+
+            gameTransport.HandShake("player");
+
+            var expectedToWrite = "{\"me\":\"player\"}";
+            A.CallTo(() => transport.Write(expectedToWrite)).MustHaveHappened();
+        }
+    }
+}
