d3bc19f772d15070533fc28e4e9564d638cdcfa7
spaceorc <spaceorc@skbkontur.ru>
2017-08-04 22:01:18 +0500

    Implement ConnectClosestMinesAi

--- lib/ConnectClosestMinesAi.cs
+++ lib/ConnectClosestMinesAi.cs
@@ -26,23 +26,103 @@ namespace lib
             var graph = new Graph(map);
 
             IMove move;
-            //if (TryExtendComponent(graph, out move))
-            //    return move;
+
+            if (TryExtendComponent(graph, out move))
+                return move;
+
             if (TryBuildNewComponent(graph, out move))
                 return move;
 
-            // foreach myMines.adjastentRivers take best
-
+            var calculator = new ConnectedCalculator(graph, punterId);
+            var maxAddScore = long.MinValue;
+            Edge bestEdge = null;
+            foreach (var vertex in graph.Vertexes.Values)
+            {
+                foreach (var edge in vertex.Edges.Where(x => x.Owner == -1))
+                {
+                    var fromMines = calculator.GetConnectedMines(edge.From);
+                    var toMines = calculator.GetConnectedMines(edge.To);
+                    long addScore;
+                    if (fromMines.Count == 0)
+                        addScore = Calc(toMines, edge.From);
+                    else 
+                    {
+                        if (toMines.Count != 0)
+                            throw new InvalidOperationException("Attempt to connect two not empty components! WTF???");
+                        addScore = Calc(fromMines, edge.To);
+                    }
+                    if (addScore > maxAddScore)
+                    {
+                        maxAddScore = addScore;
+                        bestEdge = edge;
+                    }
+                }
+            }
+            if (bestEdge != null)
+                return MakeMove(bestEdge);
+            
             return new Pass();
+        }
+
+        private long Calc(List<int> mineIds, int vertexId)
+        {
+            return mineIds.Sum(
+                mineId =>
+                {
+                    var dist = mineDistCalulator.GetDist(mineId, vertexId);
+                    return (long) dist * dist;
+                });
+        }
+
+        private bool TryExtendComponent(Graph graph, out IMove move)
+        {
+            var queue = new Queue<ExtendQueueItem>();
+            var used = new HashSet<int>();
+            foreach (var mineId in graph.Mines.Keys.Where(id => !myMines.Contains(id)))
+            {
+                var queueItem = new ExtendQueueItem
+                {
+                    CurrentVertex = graph.Vertexes[mineId],
+                    Edge = null
+                };
+                queue.Enqueue(queueItem);
+                used.Add(mineId);
+            }
+
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+                if (current.CurrentVertex.Edges.Any(x => x.Owner == punterId))
+                {
+                    move = MakeMove(current.Edge);
+                    return true;
+                }
+                foreach (var edge in current.CurrentVertex.Edges.Where(x => x.Owner == -1))
+                {
+                    var next = graph.Vertexes[edge.To];
+                    if (!used.Contains(next.Id))
+                    {
+                        var queueItem = new ExtendQueueItem
+                        {
+                            CurrentVertex = next,
+                            Edge = edge
+                        };
+                        queue.Enqueue(queueItem);
+                        used.Add(next.Id);
+                    }
+                }
+            }
+            move = null;
+            return false;
         }
 
         private bool TryBuildNewComponent(Graph graph, out IMove move)
         {
-            var queue = new Queue<QueueItem>();
-            var used = new Dictionary<int, QueueItem>();
+            var queue = new Queue<BuildQueueItem>();
+            var used = new Dictionary<int, BuildQueueItem>();
             foreach (var mineId in graph.Mines.Keys.Where(id => !myMines.Contains(id)))
             {
-                var queueItem = new QueueItem
+                var queueItem = new BuildQueueItem
                 {
                     CurrentVertex = graph.Vertexes[mineId],
                     SourceMine = graph.Vertexes[mineId],
@@ -58,7 +138,7 @@ namespace lib
                 foreach (var edge in current.CurrentVertex.Edges.Where(x => x.Owner == -1))
                 {
                     var next = graph.Vertexes[edge.To];
-                    QueueItem prev;
+                    BuildQueueItem prev;
                     if (used.TryGetValue(next.Id, out prev))
                     {
                         if (prev.SourceMine != current.SourceMine)
@@ -78,7 +158,7 @@ namespace lib
                     }
                     else
                     {
-                        var queueItem = new QueueItem
+                        var queueItem = new BuildQueueItem
                         {
                             CurrentVertex = next,
                             SourceMine = current.SourceMine,
@@ -113,11 +193,17 @@ namespace lib
             throw new NotImplementedException();
         }
 
-        private class QueueItem
+        private class BuildQueueItem
         {
             public Vertex CurrentVertex;
             public Vertex SourceMine;
             public Edge FirstEdge;
+        }
+
+        private class ExtendQueueItem
+        {
+            public Vertex CurrentVertex;
+            public Edge Edge;
         }
     }
 }
