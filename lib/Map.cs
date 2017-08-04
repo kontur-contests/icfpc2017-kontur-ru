2fbbcef6b25ef35bde4557652cb775377f738b91
Pavel Egorov <xoposhiy@mail.ru>
2017-08-04 19:57:23 +0500

    Draw rivers

--- lib/Map.cs
+++ lib/Map.cs
@@ -70,29 +70,6 @@ namespace lib
 
     public static class MapExtensions
     {
-        public static Map NormalizeCoordinates(this Map map, SizeF targetSize)
-        {
-            var box = map.GetBoundingBox();
-            return new Map(
-                map.Sites.Select(s => NormalizeCoordinates(s, box, targetSize)).ToArray(), map.Rivers, map.Mines);
-        }
-
-        private static Site NormalizeCoordinates(Site site, RectangleF bbox, SizeF targetSize)
-        {
-            return new Site(
-                site.Id, targetSize.Width * (site.X - bbox.Left) / bbox.Width,
-                targetSize.Height * (site.Y - bbox.Top) / bbox.Height);
-        }
-
-        public static RectangleF GetBoundingBox(this Map map)
-        {
-            float minX = map.Sites.Min(s => s.X);
-            float maxX = map.Sites.Max(s => s.X);
-            float minY = map.Sites.Min(s => s.Y);
-            float maxY = map.Sites.Max(s => s.Y);
-            return new RectangleF(minX, minY, Math.Max(1, maxX - minX), Math.Max(1, maxY - minY));
-        }
-
         public static bool IsMine(this Map map, int siteId)
         {
             return map.Mines.Contains(siteId);
