e1eabb189f7a02921dc8c42a75bd8e10c97442ad
yuri.okulovsky <yuri.okulovsky@gmail.com>
2017-08-05 00:03:15 +0500

    -kafka proj

--- python/icfpc/sources.py
+++ python/icfpc/sources.py
@@ -7,3 +7,16 @@ def get_dummies(count, length):
                 'Params' : { 'key_'+str(j) : i for j in range(length) }}
                     for i in range(count)  ]
 
+
+def get_dummy_ai(count):
+    fraction = 1.0/count
+    return [
+        {
+            'Name' : str(i),
+            'Params' :
+                {
+                    'Param' : (fraction*i)
+                }
+        }
+        for i in range(count)
+    ]
--- python/runner.py
+++ python/runner.py
@@ -2,9 +2,4 @@ from icfpc import *
 import json
 
 
-results = []
-with open('sample.json') as f:
-    for string in f:
-        results.append(json.loads(string))
-
-pairwise_output(results)
+print(list(one_out()))
