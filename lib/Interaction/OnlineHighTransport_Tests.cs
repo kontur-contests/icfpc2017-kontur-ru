c08aef056747eb4906974be7382998069782fbb0
s.fedyanin <s.fedyanin@skbkontur.ru>
2017-08-04 20:49:51 +0500

    Rename test

--- lib/Interaction/OnlineHighTransport_Tests.cs
+++ lib/Interaction/OnlineHighTransport_Tests.cs
@@ -6,10 +6,23 @@ using NUnit.Framework;
 namespace lib.Interaction
 {
     [TestFixture]
-    public class IGameTransport_Tests
+    public class OnlineHighTransport_Tests
     {
         [Test]
         public void TestHandShake()
+        {
+            var transport = A.Fake<ITransport>();
+            var gameTransport = new OnlineHighTransport(transport);
+            A.CallTo(() => transport.Read()).Returns("{\"you\":\"player\"}");
+
+            gameTransport.HandShake("player");
+
+            var expectedToWrite = "{\"me\":\"player\"}";
+            A.CallTo(() => transport.Write(expectedToWrite)).MustHaveHappened();
+        }
+
+        [Test]
+        public void TestReadSetup()
         {
             var transport = A.Fake<ITransport>();
             var gameTransport = new OnlineHighTransport(transport);
