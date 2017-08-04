f8946740cc3fc66fcad0d32f46fbd7a3de4534b6
kungurtsev <kungurtsev@skbkontur.ru>
2017-08-04 20:41:30 +0500

    graph from edges

--- lib/GraphImpl/Graph.cs
+++ lib/GraphImpl/Graph.cs
@@ -1,10 +1,17 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace lib.GraphImpl
 {
     public class Graph
     {
         public readonly Dictionary<int, Vertex> vertexes = new Dictionary<int, Vertex>();
+
+        public Graph()
+        {
+            
+        }
 
         public Graph(Map map)
         {
@@ -17,6 +24,19 @@ namespace lib.GraphImpl
                 if (river.Source != river.Target)
                     vertexes[river.Target].Edges.Add(new Edge(river.Source, river.Owner));
             }
+        }
+
+        public void AddVertex(int v, bool isMine = false)
+        {
+            if (vertexes.ContainsKey(v))
+                return;
+            vertexes.Add(v, new Vertex(v, isMine));
+        }
+
+        public void AddEdge(int v, int u)
+        {
+            vertexes[v].Edges.Add(new Edge(u, -1));
+            vertexes[u].Edges.Add(new Edge(v, -1));
         }
     }
 }
