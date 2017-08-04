3e3973dc51c41582aac09428035102ed5756e35a
Igor Lukanin <mail@igor.lukanin.name>
2017-08-04 22:13:13 +0500

    tests

--- worker/Strategy.cs
+++ worker/Strategy.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using NUnit.Framework;
 
 namespace worker
 {
@@ -17,40 +16,6 @@ namespace worker
             return players
                 .OrderByDescending(player => player.Params.Values.Sum())
                 .Select((player, i) => Tuple.Create(player, i));
-        }
-    }
-
-    [TestFixture]
-    public class StrategyTests
-    {
-        [Test]
-        public void DummySumPlayerStrategy_Should()
-        {
-            var strategy = new DummySumPlayerStrategy();
-            var playersIn = new List<PlayerWithParams>
-            {
-                new PlayerWithParams
-                {
-                    Name = "zero",
-                    Params = new Dictionary<string, double> {{"param1", 0}, {"param2", 0}, {"param3", 0}}
-                },
-                new PlayerWithParams
-                {
-                    Name = "two",
-                    Params = new Dictionary<string, double> {{"param1", 1}, {"param2", 0}, {"param3", 1}}
-                },
-                new PlayerWithParams
-                {
-                    Name = "one",
-                    Params = new Dictionary<string, double> {{"param1", 0}, {"param2", 0}, {"param3", 1}}
-                },
-            };
-
-            var playersOut = strategy.Play(playersIn);
-            var bestPlayerOut = playersOut.First().Item1;
-            
-            Assert.AreEqual(2, bestPlayerOut.Rank);
-            Assert.AreEqual("two", bestPlayerOut.Name);
         }
     }
 }
