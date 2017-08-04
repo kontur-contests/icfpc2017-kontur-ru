63e9d8067aa82f0b774c39bd1d8e424f0a4f81d6
khr <khr@kontur.ru>
2017-08-04 19:41:46 +0500

    ScoreBuilder base

---
+++ lib/Strategies/ScoreBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lib.Strategies
+{
+	class SimpleScoreBuilder
+	{
+		public static Dictionary<int, int[]> GetScores(Map map)
+		{
+			
+		}
+
+		public static int[] GetScoresFromMine(Map map, int mine)
+		{
+		}
+	}
+}
