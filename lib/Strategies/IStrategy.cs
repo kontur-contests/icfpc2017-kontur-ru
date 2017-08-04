69fb63b7d0760777049e49fb7f85b8d4ce1f6ab7
spaceorc <spaceorc@skbkontur.ru>
2017-08-04 18:09:09 +0500

    Add IStrategy

---
+++ lib/Strategies/IStrategy.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace lib.Strategies
+{
+    public interface IStrategy
+    {
+        void Init(Map map);
+        List<TurnResult> Turn(Map map);
+    }
+}
