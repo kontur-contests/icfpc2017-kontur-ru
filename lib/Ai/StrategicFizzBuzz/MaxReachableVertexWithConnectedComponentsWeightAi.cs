9f5011dd9f6d66d817123d705df5ed67841d5887
msamoylenko <ghost1212@mail.ru>
2017-08-05 21:10:20 +0500

    push until pizdec

---
+++ lib/Ai/StrategicFizzBuzz/MaxReachableVertexWithConnectedComponentsWeightAi.cs
@@ -0,0 +1,16 @@
+using JetBrains.Annotations;
+using lib.Strategies.EdgeWeighting;
+
+namespace lib.Ai.StrategicFizzBuzz
+{
+    [UsedImplicitly]
+    public class MaxReachableVertexWithConnectedComponentsWeightAi : EdgeWeightingStrategicAi
+    {
+        public override string Name => nameof(MaxReachableVertexWithConnectedComponentsWeightAi);
+
+        protected override IEdgeWeighter CreateEdgeWeighter(int punterId, int puntersCount, Map map, Settings settings)
+        {
+            return new MaxVertextWeighterWithConnectedComponents(map);
+        }
+    }
+}
