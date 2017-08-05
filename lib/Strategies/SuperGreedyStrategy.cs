9aa1901945f08005103c465a4eee476d173b5c7e
khr <khr@kontur.ru>
2017-08-05 19:42:56 +0500

    Компоненты связности

--- lib/Strategies/SuperGreedyStrategy.cs
+++ lib/Strategies/SuperGreedyStrategy.cs
@@ -10,20 +10,52 @@ namespace lib.Strategies
 {
     class SuperGreedyStrategy : IStrategy
     {
-        public Graph Graph;
-        public ShortestPathGraph ShortestPathGraph;
         private MineDistCalculator MineDistCalulator;
 
-        public SuperGreedyStrategy(Graph graph, ShortestPathGraph shortestPathGraph, MineDistCalculator mineDistCalulator)
+        public SuperGreedyStrategy(ShortestPathGraph shortestPathGraph, MineDistCalculator mineDistCalulator)
         {
-            Graph = graph;
-            ShortestPathGraph = shortestPathGraph;
             MineDistCalulator = mineDistCalulator;
         }
 
         public List<TurnResult> Turn(Graph graph)
         {
             throw new NotImplementedException();
+        }
+    }
+
+    class ConnectedComponent
+    {
+        public HashSet<int> Vertices = new HashSet<int>();
+        public HashSet<int> Mines = new HashSet<int>();
+
+        public ConnectedComponent(Graph graph, int owner)
+        {
+            var queue = new Queue<Vertex>();
+
+            foreach (var mine in graph.Mines)
+            {
+                if(mine.Value.Edges.All(edge => edge.Owner == owner))
+                    break;
+
+                queue.Clear();
+
+                Vertices.Add(mine.Key);
+                Mines.Add(mine.Key);
+                queue.Enqueue(mine.Value);
+
+                while (queue.Count > 0)
+                {
+                    var node = queue.Dequeue();
+                    foreach (var edge in node.Edges.Where(edge => edge.Owner == owner).Where(edge => !Vertices.Contains(edge.To)))
+                    {
+                        var edgeNode = graph.Vertexes[edge.To];
+                        if (edgeNode.IsMine)
+                            Mines.Add(edgeNode.Id);
+                        Vertices.Add(edgeNode.Id);
+                        queue.Enqueue(edgeNode);
+                    }
+                }
+            }
         }
     }
 }
