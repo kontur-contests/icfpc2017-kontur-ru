92e264602c6661ad41343e6621acab9157685966
khr <khr@kontur.ru>
2017-08-05 17:53:58 +0500

    LochKiller

--- lib/Ai/LochKillerAi.cs
+++ lib/Ai/LochKillerAi.cs
@@ -7,31 +7,51 @@ using lib.GraphImpl;
 
 namespace lib.Ai
 {
-    class LochKillerAi : ConnectClosestMinesAi
+    class LochKillerAi : IAi
     {
-        public new string Name => nameof(LochKillerAi);
+        public string Name => nameof(LochKillerAi);
 
-        private readonly int PunterId;
-        public Graph Graph;
+        private GreedyAi Base = new GreedyAi();
 
-        Random rand = new Random();
-
-        public LochKillerAi(Graph graph, int panterId)
+        public Future[] StartRound(int punterId, int puntersCount, Map map, Settings settings)
         {
-            Graph = graph;
-            PunterId = panterId;
+            return Base.StartRound(punterId, puntersCount, map, settings);
         }
 
-        public new Move GetNextMove(Move[] prevMoves, Map map)
+        public Move GetNextMove(Move[] prevMoves, Map map)
         {
-            var nearMinesEdge = map.Mines
-                .Select(mine => new{mine, edges = Graph.Vertexes[mine].Edges.Select(edge => edge.River).ToList()})
-                .OrderBy(mine => Tuple.Create(mine.edges.Select(edge => edge.Owner).Distinct().Count(), rand.Next()))
-                .SelectMany(mine => mine.edges)
-                .FirstOrDefault(edge => edge.Owner < 0);
-            if (nearMinesEdge == null)
-                return base.GetNextMove(prevMoves, map);
-            return new ClaimMove(PunterId, nearMinesEdge.Source, nearMinesEdge.Target);
+            try
+            {
+                Random rand = new Random(1031);
+                var graph = new Graph(map);
+
+                var nearMinesEdge = map.Mines
+                    .Select(mine => new { mine, edges = graph.Vertexes[mine].Edges.Select(edge => edge.River).ToList() })
+                    .OrderBy(mine => Tuple.Create(mine.edges.Select(edge => edge.Owner).Distinct().Count(), rand.Next()))
+                    .Where(mine => mine.edges.Count <= 100)
+                    .SelectMany(mine => mine.edges)
+                    .FirstOrDefault(edge => edge.Owner < 0);
+                if (nearMinesEdge == null)
+                    return Base.GetNextMove(prevMoves, map);
+                return new ClaimMove(Base.punterId, nearMinesEdge.Source, nearMinesEdge.Target);
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public string SerializeGameState()
+        {
+            return Base.SerializeGameState();
+        }
+
+        public void DeserializeGameState(string gameState)
+        {
+            Base.DeserializeGameState(gameState);
         }
     }
 }
