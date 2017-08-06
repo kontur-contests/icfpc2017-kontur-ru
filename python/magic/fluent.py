d3703681553198dc2c860ea721f87c39c7767d05
t.efimov <t.efimov@skbkontur.ru>
2017-08-06 15:04:58 +0500

    Refactor store_pointwise

--- python/magic/fluent.py
+++ python/magic/fluent.py
@@ -99,7 +99,21 @@ class Fluent:
     def store_pointwise(self, filename, mode='w'):
         keys = self.param_names
         with open(filename, mode) as file:
-            file.write('game_number,server_name,scores,ranking,tournament_scores,num_players,map,map_rivers_count,map_sites_count,map_mines_count,name,')
+            if mode == 'w':
+                # file.write('game_number,server_name,scores,ranking,tournament_scores,num_players,map,map_rivers_count,map_sites_count,map_mines_count,name,')
+                file.write(','.join([
+                    'game_number',
+                    'server_name',
+                    'scores',
+                    'ranking',
+                    'tournament_scores',
+                    'num_players',
+                    'map',
+                    'map_rivers_count',
+                    'map_sites_count',
+                    'map_mines_count',
+                    'name'
+                ]))
             file.write(",".join(keys))
             file.write('\n')
             for game_number, game in enumerate(self.results):
