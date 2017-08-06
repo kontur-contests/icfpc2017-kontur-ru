ce09419a0a0f020c784b919a8b87bb2175b85fd4
msamoylenko <ghost1212@mail.ru>
2017-08-06 19:08:07 +0500

    Made LochKillerAi to strategic.

---
+++ lib/Ai/StrategicFizzBuzz/LochKillerAi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using lib.Strategies;
+using lib.Strategies.EdgeWeighting;
+
+namespace lib.Ai.StrategicFizzBuzz
+{
+    public class LochKillerAi : CompositeStrategicAi
+    {
+        public LochKillerAi()
+            : base(
+                (state, services) => new AllComponentsEWStrategy(new LochKillerEdgeWeighter(state, services), state, services),
+                (state, services) => new GreedyStrategy(state, services, Math.Max))
+        {
+        }
+
+        public override string Version => "0.1.1";
+    }
+}
