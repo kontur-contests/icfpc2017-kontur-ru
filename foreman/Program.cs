fca88a9699b541bc7d4275e7f520da50b8e77e2b
Alexey Kirpichnikov <alexkir@skbkontur.ru>
2017-08-06 17:37:17 +0500

    add foreman project

---
+++ foreman/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+using lib.OnlineRunner;
+using NLog;
+
+namespace foreman
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var logger = LogManager.GetCurrentClassLogger();
+            var config = new Dictionary<string, object>
+            {
+                { "group.id", "icfpc2017-foreman" },
+                { "bootstrap.servers", "icfpc-broker.dev.kontur.ru" }
+            };
+            using (var producer = new Producer<Null, string>(config, null, new StringSerializer(Encoding.UTF8)))
+            {
+                var match = OnlineArenaRunner.GetNextMatch();
+                var deliveryReport = producer.ProduceAsync("matches", null, match.Port.ToString());
+
+                deliveryReport.ContinueWith(
+                    x =>
+                    {
+                        logger.Info(
+                            $"Sent result | Partition: {x.Result.Partition}, Offset: {x.Result.Offset}");
+                    });
+            }
+        }
+    }
+}
