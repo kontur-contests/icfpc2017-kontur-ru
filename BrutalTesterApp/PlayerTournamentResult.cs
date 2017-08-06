6d41ddbde392eb0b7c423ac821fdb98f3dcb7f98
Pavel Egorov <xoposhiy@mail.ru>
2017-08-06 18:34:04 +0500

    BrutalTester shows Average Normalized Match Score ( = place / playersCount)

--- BrutalTesterApp/PlayerTournamentResult.cs
+++ BrutalTesterApp/PlayerTournamentResult.cs
@@ -13,6 +13,11 @@ namespace ConsoleApp
         public int GamesPlayed;
         public int GamesWon;
         public int ExceptionsCount;
+        public double FuturesCompletedCount;
+        public double FuturesCount;
+        public double FuturesCompletementRate => FuturesCompletedCount / FuturesCount;
+        public double NormalizedMatchScoresSum;
+        public double AvNormalizedMatchScores => NormalizedMatchScoresSum / GamesPlayed;
         public double WinRate => GamesWon / (double)GamesPlayed;
     }
 }
