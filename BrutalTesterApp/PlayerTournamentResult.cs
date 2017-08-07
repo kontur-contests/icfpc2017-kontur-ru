b4739142070d8c42bfe69e8ef97c22bd895d4081
Anton Tolstov <a.tolstov@skbkontur.ru>
2017-08-07 13:03:21 +0500

    Brutal test: merge tests

--- BrutalTesterApp/PlayerTournamentResult.cs
+++ BrutalTesterApp/PlayerTournamentResult.cs
@@ -1,13 +1,16 @@
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
 using lib;
 
 namespace ConsoleApp
 {
     public class PlayerTournamentResult
     {
-        public PlayerTournamentResult(AiFactory factory)
+        public PlayerTournamentResult(AiFactory factory = null)
         {
             Factory = factory;
+            Maps = new HashSet<string>();
         }
 
         public AiFactory Factory;
@@ -17,13 +20,56 @@ namespace ConsoleApp
         public StatValue NormalizedMatchScores = new StatValue();
         public StatValue GainFuturesScoreRate = new StatValue();
         public StatValue GainFuturesCountRate = new StatValue();
+        public HashSet<string> Maps;
+
         public StatValue TurnTime = new StatValue
         {
             ShowDispersion = true
         };
+
         public StatValue OptionUsageRate = new StatValue
         {
             ShowDispersion = true
         };
+
+        public PlayerTournamentResult Clone()
+        {
+            return new PlayerTournamentResult(Factory)
+            {
+                ExceptionsCount = ExceptionsCount,
+                GamesPlayed = GamesPlayed,
+                GamesWon = GamesWon.Clone(),
+                NormalizedMatchScores = NormalizedMatchScores.Clone(),
+                GainFuturesCountRate = GainFuturesCountRate.Clone(),
+                GainFuturesScoreRate = GainFuturesScoreRate.Clone(),
+                TurnTime = TurnTime.Clone(),
+                OptionUsageRate = OptionUsageRate.Clone()
+            };
+        }
+
+        public static IEnumerable<PlayerTournamentResult> Merge(IEnumerable<PlayerTournamentResult> results)
+        {
+            return results.GroupBy(r => r.Factory.Name).Select(MergeGroup);
+        }
+
+        private static PlayerTournamentResult MergeGroup(IEnumerable<PlayerTournamentResult> results)
+        {
+            var r = new PlayerTournamentResult(null);
+            foreach (var result in results)
+            {
+                r.Factory = result.Factory;
+                r.GamesPlayed += result.GamesPlayed;
+                r.ExceptionsCount += result.ExceptionsCount;
+                r.GamesWon.AddAll(result.GamesWon);
+                r.NormalizedMatchScores.AddAll(result.NormalizedMatchScores);
+                r.GainFuturesScoreRate.AddAll(result.GainFuturesScoreRate);
+                r.GainFuturesCountRate.AddAll(result.GainFuturesCountRate);
+                foreach (var map in result.Maps)
+                {
+                    r.Maps.Add(map);
+                }
+            }
+            return r;
+        }
     }
 }
